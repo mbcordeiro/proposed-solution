90b57672d664dff305525fa93bad0741fd8c1973
mbcordeiro <matheusdebarroscordeiro@gmail.com>
2021-03-25 21:36:25 -0300

    Update service

--- fast-service-radis/src/api/api.js
+++ fast-service-radis/src/api/api.js
@@ -1,5 +1,6 @@
 const axios = require('axios')
-const token = 'BQA0U2rfYoOV8P4WI13u3rlnoVim-50WYHODufps0paCXpFq7aWhqQrrtB-xtZcGXVQvejtfflijcD2XFPmRKsFhCdlUexMojyATbeA7UWPj17kjwSNOH2gjXDkRHHqvICRYT__yPPmL6cYmZd_t1RRHG3U1EsFl5u1Hv_t35ps4Bp_bomIIo9zpH52B5NHrg70SGssKRf5gxrkQWHlzC04CmOaZT3xODgTNsuJ0aNYjJCJy6GGU7EMYZlDbwBUACLSf8z-tntRamL6JmqQ5LtHl8hz78H2Kkfxx6T7-'
+const token =
+  'BQA0U2rfYoOV8P4WI13u3rlnoVim-50WYHODufps0paCXpFq7aWhqQrrtB-xtZcGXVQvejtfflijcD2XFPmRKsFhCdlUexMojyATbeA7UWPj17kjwSNOH2gjXDkRHHqvICRYT__yPPmL6cYmZd_t1RRHG3U1EsFl5u1Hv_t35ps4Bp_bomIIo9zpH52B5NHrg70SGssKRf5gxrkQWHlzC04CmOaZT3xODgTNsuJ0aNYjJCJy6GGU7EMYZlDbwBUACLSf8z-tntRamL6JmqQ5LtHl8hz78H2Kkfxx6T7-'
 const api = axios.create({
   baseURL: 'https://api.spotify.com/v1',
   headers: {
--- fast-service-radis/src/api/spotify.js
+++ fast-service-radis/src/api/spotify.js
@@ -1,34 +1,30 @@
-const Redis = require('ioredis')
+const api = require('./api')
+const querystring = require('querystring')
+const cache = require('./cache')
 
-class cache {
-  constructor() {
-    this.redis = new Redis({
-      host: process.env.REDIS_HOST || 'localhost',
-      port: process.env.REDIS_PORT || 6379,
-      keyPrefix: 'cache:',
+class Spotify {
+  async recommendation(options) {
+    const params = querystring.stringify(options)
+
+    const cached = await cache.get(params)
+
+    if (cached) {
+      return cached
+    }
+
+    const { data } = await api.get(`/recommendations?${params}`)
+
+    const response = data.tracks.map(album => {
+      return {
+        artist: album.artists[0].name,
+        url: album.artists[0].external_urls.spotify,
+      }
     })
-  }
 
-  async get(key) {
-    const value = await this.redis.get(key)
+    cache.set(params, response, 60 * 15)
 
-    return value ? JSON.parse(value) : null
-  }
-
-  set(key, value, timeExp) {
-    return this.redis.set(key, JSON.stringify(value), 'EX', timeExp)
-  }
-
-  del(key) {
-    return this.redis.del(key)
-  }
-
-  async delPrefix(prefix) {
-    const keys = (await this.redis.keys(`cache:${prefix}:*`)).map(key =>
-      key.replace('cache:', ''),
-    )
-
-    return this.redis.del(keys)
+    return response
   }
 }
-module.exports = new cache()
+
+module.exports = new Spotify()
